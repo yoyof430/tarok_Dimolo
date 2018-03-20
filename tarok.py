6da0a5dbb70cbe689b611a658ecc1afcb163982a
Dimitar Stoilkov <dimitar.stoilkov@outlook.com>
2018-03-20 20:16:37 +0100

    Slika

--- tarok.py
+++ tarok.py
@@ -108,8 +108,7 @@ class Cela_igra():
         self.karte_talon = self.karte_talon.difference(self.karte_rac1)
         self.karte_rac2= random.sample(self.karte_talon, 16)
         self.karte_talon = self.karte_talon.difference(self.karte_rac2)
-        self.klikGumba = True
-        self.canvas.delete(self.gumb_window)
+        self.canvas.delete(self.gumb_window) #pobriše gumb, potem ko je kliknjen
         print(self.karte_igralec,'\n',self.karte_rac1,'\n',self.karte_rac2,'\n',self.karte_talon)
 
 
