3925677fc22942517964b6664235b377293477e0
Dimitar Stoilkov <dimitar.stoilkov@outlook.com>
2018-03-27 12:07:05 +0200

    Slike

---
+++ karte.py
@@ -0,0 +1,76 @@
+from tkinter import *
+class Karte():
+    def __init__(self,slika, barva, vrednost, moc):
+        self.barva = barva
+        self.vrednost = vrednost
+        self.moc = moc
+        self.slika = PhotoImage(file = slika)
+
+    def __lt__(self, other):
+        return self.moc<other.moc
+
+    def __repr__(self):
+        return self.barva+' '+str(self.moc)
+
+
+def ustvari_karte():
+    T1=Karte('./Slike/Karte/T1.pbm', barva = 'tarok', vrednost= 5, moc = 11)
+    T2=Karte('./Slike/Karte/T2.pbm', barva = 'tarok', vrednost= 1, moc = 12)
+    T3=Karte('./Slike/Karte/T3.pbm', barva = 'tarok', vrednost= 1, moc = 13)
+    T4=Karte('./Slike/Karte/T4.pbm', barva = 'tarok', vrednost= 1, moc = 14)
+    T5=Karte('./Slike/Karte/T5.pbm', barva = 'tarok', vrednost= 1, moc = 15)
+    T6=Karte('./Slike/Karte/T6.pbm', barva = 'tarok', vrednost= 1, moc = 16)
+    T7=Karte('./Slike/Karte/T7.pbm', barva = 'tarok', vrednost= 1, moc = 17)
+    T8=Karte('./Slike/Karte/T8.pbm', barva = 'tarok', vrednost= 1, moc = 18)
+    T9=Karte('./Slike/Karte/T9.pbm', barva = 'tarok', vrednost= 1, moc = 19)
+    T10=Karte('./Slike/Karte/T10.pbm', barva = 'tarok', vrednost= 1, moc = 20)
+    T11=Karte('./Slike/Karte/T11.pbm', barva = 'tarok', vrednost= 1, moc = 21)
+    T12=Karte('./Slike/Karte/T12.pbm', barva = 'tarok', vrednost= 1, moc = 22)
+    T13=Karte('./Slike/Karte/T13.pbm', barva = 'tarok', vrednost= 1, moc = 23)
+    T14=Karte('./Slike/Karte/T14.pbm', barva = 'tarok', vrednost= 1, moc = 24)
+    T15=Karte('./Slike/Karte/T15.pbm', barva = 'tarok', vrednost= 1, moc = 25)
+    T16=Karte('./Slike/Karte/T16.pbm', barva = 'tarok', vrednost= 1, moc = 26)
+    T17=Karte('./Slike/Karte/T17.pbm', barva = 'tarok', vrednost= 1, moc = 27)
+    T18=Karte('./Slike/Karte/T18.pbm', barva = 'tarok', vrednost= 1, moc = 28)
+    T19=Karte('./Slike/Karte/T19.pbm', barva = 'tarok', vrednost= 1, moc = 29)
+    T20=Karte('./Slike/Karte/T20.pbm', barva = 'tarok', vrednost= 1, moc = 30)
+    T21=Karte('./Slike/Karte/T21.pbm', barva = 'tarok', vrednost= 5, moc = 31)
+    T22=Karte('./Slike/Karte/T22.pbm', barva = 'tarok', vrednost= 5, moc = 32)
+    S1=Karte('./Slike/Karte/S1.pbm', barva = 'pik', vrednost= 1, moc = 1)
+    S2=Karte('./Slike/Karte/S2.pbm', barva = 'pik', vrednost= 1, moc = 2)
+    S3=Karte('./Slike/Karte/S3.pbm', barva = 'pik', vrednost= 1, moc = 3)
+    S4=Karte('./Slike/Karte/S4.pbm', barva = 'pik', vrednost= 1, moc = 4)
+    S5=Karte('./Slike/Karte/S5.pbm', barva = 'pik', vrednost= 2, moc = 5)
+    S6=Karte('./Slike/Karte/S6.pbm', barva = 'pik', vrednost= 3, moc = 6)
+    S7=Karte('./Slike/Karte/S7.pbm', barva = 'pik', vrednost= 4, moc = 7)
+    S8=Karte('./Slike/Karte/S8.pbm', barva = 'pik', vrednost= 5, moc = 8)
+    H1=Karte('./Slike/Karte/H1.pbm', barva = 'srce', vrednost= 1, moc = 1)
+    H2=Karte('./Slike/Karte/H2.pbm', barva = 'srce', vrednost= 1, moc = 2)
+    H3=Karte('./Slike/Karte/H3.pbm', barva = 'srce', vrednost= 1, moc = 3)
+    H4=Karte('./Slike/Karte/H4.pbm', barva = 'srce', vrednost= 1, moc = 4)
+    H5=Karte('./Slike/Karte/H5.pbm', barva = 'srce', vrednost= 2, moc = 5)
+    H6=Karte('./Slike/Karte/H6.pbm', barva = 'srce', vrednost= 3, moc = 6)
+    H7=Karte('./Slike/Karte/H7.pbm', barva = 'srce', vrednost= 4, moc = 7)
+    H8=Karte('./Slike/Karte/H8.pbm', barva = 'srce', vrednost= 5, moc = 8)
+    D1=Karte('./Slike/Karte/D1.pbm', barva = 'karo', vrednost= 1, moc = 1)
+    D2=Karte('./Slike/Karte/D2.pbm', barva = 'karo', vrednost= 1, moc = 2)
+    D3=Karte('./Slike/Karte/D3.pbm', barva = 'karo', vrednost= 1, moc = 3)
+    D4=Karte('./Slike/Karte/D4.pbm', barva = 'karo', vrednost= 1, moc = 4)
+    D5=Karte('./Slike/Karte/D5.pbm', barva = 'karo', vrednost= 2, moc = 5)
+    D6=Karte('./Slike/Karte/D6.pbm', barva = 'karo', vrednost= 3, moc = 6)
+    D7=Karte('./Slike/Karte/D7.pbm', barva = 'karo', vrednost= 4, moc = 7)
+    D8=Karte('./Slike/Karte/D8.pbm', barva = 'karo', vrednost= 5, moc = 8)
+    C1=Karte('./Slike/Karte/C1.pbm', barva = 'križ', vrednost= 1, moc = 1)
+    C2=Karte('./Slike/Karte/C2.pbm', barva = 'križ', vrednost= 1, moc = 2)
+    C3=Karte('./Slike/Karte/C3.pbm', barva = 'križ', vrednost= 1, moc = 3)
+    C4=Karte('./Slike/Karte/C4.pbm', barva = 'križ', vrednost= 1, moc = 4)
+    C5=Karte('./Slike/Karte/C5.pbm', barva = 'križ', vrednost= 2, moc = 5)
+    C6=Karte('./Slike/Karte/C6.pbm', barva = 'križ', vrednost= 3, moc = 6)
+    C7=Karte('./Slike/Karte/C7.pbm', barva = 'križ', vrednost= 4, moc = 7)
+    C8=Karte('./Slike/Karte/C8.pbm', barva = 'križ', vrednost= 5, moc = 8)
+    return{S1,S2,S3,S4,S5,S6,S7,S8,H1,H2,H3,H4,H5,H6,H7,H8,D1,D2,D3,D4,D5,D6,D7,D8,C1,C2,C3,C4,C5,C6,C7,C8,T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,T17,T18,T19,T20,T21,T22}
+
+#print(type(C8))
+
+
+
--- tarok.py
+++ tarok.py
@@ -3,80 +3,24 @@
 from tkinter import *
 from tkinter.font import Font
 from winsound import *
+from karte import *
+from math import *
 import random
 
+#print(type(karte))
 root = Tk()
 
 
-T1='XT1'
-T2='XT2'
-T3='XT3'
-T4='XT4'
-T5='XT5'
-T6='XT6'
-T7='XT7'
-T8='XT8'
-T9='XT9'
-T10='XT10'
-T11='XT11'
-T12='XT12'
-T13='XT13'
-T14='XT14'
-T15='XT15'
-T16='XT16'
-T17='XT17'
-T18='XT18'
-T19='XT19'
-T20='XT20'
-T21='XT21'
-T22='XT22'
-S1='XS1'
-S2='XS2'
-S3='XS3'
-S4='XS4'
-S5='XS5'
-S6='XS6'
-S7='XS7'
-S8='XS8'
-H1='XH1'
-H2='XH2'
-H3='XH3'
-H4='XH4'
-H5='XH5'
-H6='XH6'
-H7='XH7'
-H8='XH8'
-D1='XD1'
-D2='XD2'
-D3='XD3'
-D4='XD4'
-D5='XD5'
-D6='XD6'
-D7='XD7'
-D8='XD8'
-C1='XC1'
-C2='XC2'
-C3='XC3'
-C4='XC4'
-C5='XC5'
-C6='XC6'
-C7='XC7'
-C8='XC8'
-
-karte = set([T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,T17,T18,T19,T20,T21,T22,S1,S2,S3,S4,S5,S6,S7,S8,H1,H2,H3,H4,H5,H6,H7,H8,D1,D2,D3,D4,D5,D6,D7,D8,C1,C2,C3,C4,C5,C6,C7,C8])
 
 class Cela_igra():
     def __init__(self, master):
         self.master = master
         self.canvas = Canvas(master, width = 1200, height = 700)
-        self.ozdaje = PhotoImage(file = 'Slike/LesenoOzadje.png')
+        self.ozadje = PhotoImage(file = 'Slike/LesenoOzadje.png')
         self.napis = PhotoImage(file = 'Slike/ZnakFinal.png')
-        self.canvas.create_image(600,350, image=self.ozdaje)
+        self.canvas.create_image(600,350, image=self.ozadje)
         self.canvas.create_image(600,200, image = self.napis, tag = 'napis')
         self.font = Font(family='Western Normal', size = 16)
-        #self.zvok = lambda: PlaySound('Slike/Zvok.wav', SND_FILENAME)
-
-        self.klikGumba = False
 
 
         self.canvas.pack()
@@ -86,7 +30,7 @@ class Cela_igra():
         self.karte_igralec =set()
         self.karte_rac1=set()
         self.karte_rac2=set()
-        self.karte_talon=karte
+        self.karte_talon=set()
         meni = Menu(self.master)
         self.master.config(menu=meni)
         self.gumb = Button(self.canvas, command = self.razdeli_karte)
@@ -105,11 +49,34 @@ class Cela_igra():
         frame = Frame(root, width=600, height=600)
         frame.pack()
 
+    def razvrsti_kart(self,seznam):
+        sl = {'križ':[],'srce':[],'pik':[],'karo':[],'tarok':[]}
+        for el in seznam:
+            sl[el.barva].append(el)
+        return sl
+
+    def prikazi_karte(self,sl):
+        x = 200
+        y = 600
+        fi = 135
+        dfi = 90/(len(self.karte_igralec)-2)
+        for sez in sl.values():
+            sez.sort()
+            for el in sez:
+                self.canvas.create_image(x, y, image=el.slika)
+                x += 60*sin(radians(fi))
+                y += 30*cos(radians(fi))
+                fi -= dfi
+
+
+
 
     def razdeli_karte(self):
         PlaySound('Slike/Zvok.wav', SND_ASYNC)
-        self.karte_talon = karte
+        self.karte_talon = ustvari_karte()
         self.karte_igralec=random.sample(self.karte_talon,16)
+        self.prikazi_karte(self.razvrsti_kart(self.karte_igralec))
+        self.razvrsti_kart(self.karte_igralec)
         self.karte_talon=self.karte_talon.difference(self.karte_igralec)
         self.karte_rac1 = random.sample(self.karte_talon, 16)
         self.karte_talon = self.karte_talon.difference(self.karte_rac1)
@@ -118,43 +85,12 @@ class Cela_igra():
         self.canvas.delete(self.gumb_window, 'napis') #pobriše gumb, potem ko je kliknjen
 
 
-        #print(self.karte_igralec,'\n',self.karte_rac1,'\n',self.karte_rac2,'\n',self.karte_talon)
-
-
-
-    def nova_igra(self):
-        self.rac = 0
-        self.igralec = 0
-        self.rezultat = ""
-        self.pripravi_canvas()
-        self.posodobi()
-
-    def shrani(self):
-        with open("lizard.txt", 'w') as a:
-            print('{}\n{}'.format(self.igralec, self.rac), file=a)
-
-    def nalozi(self):
-        sez = []
-        with open("lizard.txt", 'r') as b:
-            for i in b:
-                x = i.strip()
-                sez.append(int(x))
-            self.igralec = sez[0]
-            self.rac = sez[1]
-            self.rezultat = ""
-        self.posodobi()
-        self.pripravi_canvas()
-
-    def posodobi(self):
-        self.napis_rezultat.set(str(self.rezultat))
-        self.napis_tock_igralec.set("igralec: %s" % str(self.igralec))
-        self.napis_tock_rac.set("računalnik:%s" % str(self.rac))
-
+        print(self.karte_igralec,'\n',self.karte_rac1,'\n',self.karte_rac2,'\n',self.karte_talon)
 
 
 
 
 
 aplikacija = Cela_igra(root)
-root.state('zoomed') #windowed
+#root.state('zoomed') #windowed
 root.mainloop()
